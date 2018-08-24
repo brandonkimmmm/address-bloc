89e9cad1cdda3cd2ab63431fc6f88d7323e02456
Brandon Kim <bkim2490@gmail.com>
2018-08-24 13:14:15 +0900

    prompt-and-menu-assignment: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,7 +9,19 @@ module.exports = class MenuController {
         message: 'Please choose from an option below: ',
         choices: [
           'Add new contact',
+          'Get the date',
           'Exit'
+        ]
+      }
+    ];
+    this.dateQuestions = [
+      {
+        type: 'list',
+        name: 'dayAndTimeChoice',
+        message: 'Would you like to know the day and time? ',
+        choices: [
+          'Yes',
+          'No'
         ]
       }
     ];
@@ -22,6 +34,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice){
         case 'Add new contact':
           this.addContact();
+          break;
+        case 'Get the date':
+          this.getDate();
           break;
         case 'Exit':
           this.exit();
@@ -43,6 +58,52 @@ module.exports = class MenuController {
     this.clear();
     console.log('addContact called');
     this.main();
+  }
+
+  getDate(){
+    let date = new Date();
+    let dateArray = date.getMonth() + ' ' + date.getDate() + ', ' + date.getFullYear();
+    inquirer.prompt(this.dateQuestions).then((response) => {
+      switch(response.dayAndTimeChoice){
+        case 'Yes':
+          let day = date.getDay();
+          switch(day){
+            case 0:
+              day = 'Sunday';
+              break;
+            case 1:
+              day = 'Monday';
+              break;
+            case 2:
+              day = 'Tuesday';
+              break;
+            case 3:
+              day = 'Wednesday';
+              break;
+            case 4:
+              day = 'Thursday';
+              break;
+            case 5:
+              day = 'Friday';
+              break;
+            case 6:
+              day = 'Saturday'
+              break;
+          }
+          let minutes = date.getMinutes();
+          let time = date.getHours() + ':' + minutes + ':' + date.getSeconds();
+          dateArray += ' ' + day + ' ' + time;
+          this.clear();
+          console.log(dateArray);
+          this.main();
+          break;
+        case 'No':
+          this.clear();
+          console.log(dateArray);
+          this.main();
+          break;
+      }
+    })
   }
 
   exit(){
